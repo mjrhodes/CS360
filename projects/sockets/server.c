bd9b3d02007f83b57226560654ef0378147d22ae
mjrhodes <mjrhodes22@gmail.com>
2016-03-21 05:47:53 +0000

    first commit

---
+++ projects/sockets/server.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[])
+{
+	socket(AF_INET,SOCK_
